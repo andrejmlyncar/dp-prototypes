d8360a038abfa23bdbc0ab86873774f83f1e0b04
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-10-25 21:00:27 +0200

    IsSubtree method reworked - now all tests results are passed. More tests required to proof method full functionality.

--- GraphComparison/src/main/java/com/mlyncar/dp/prototypes/graphcomparison/service/impl/ComparisonServiceImpl.java
+++ GraphComparison/src/main/java/com/mlyncar/dp/prototypes/graphcomparison/service/impl/ComparisonServiceImpl.java
@@ -8,12 +8,16 @@ package com.mlyncar.dp.prototypes.graphcomparison.service.impl;
 import com.mlyncar.dp.prototypes.graphcomparison.service.ComparisonService;
 import com.mlyncar.dp.prototypes.graphdesign.entity.Graph;
 import com.mlyncar.dp.prototypes.graphdesign.entity.Node;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Andrej Mlyncar <a.mlyncar@gmail.com>
  */
 public class ComparisonServiceImpl implements ComparisonService {
+
+    private final Logger logger = LoggerFactory.getLogger(ComparisonServiceImpl.class);
 
     @Override
     public boolean isGraphSubgraph(Graph referenceTree, Graph subTree) {
@@ -30,17 +34,23 @@ public class ComparisonServiceImpl implements ComparisonService {
         if (rootReferenceNode == null) {
             return false;
         }
+        logger.debug("Starting to compare nodes {} {} ", rootReferenceNode.getName(), rootSubTreeNode.getName());
 
         if (rootReferenceNode.isNodeEqual(rootSubTreeNode)) {
+            logger.debug("Nodes {} {} and {} {} are equal", rootReferenceNode.getId(), rootReferenceNode.getName(), rootSubTreeNode.getId(), rootSubTreeNode.getName());
             if (rootReferenceNode.isLeaf()) {
+                logger.debug("Node {} is leaf", rootReferenceNode.getName());
                 return true;
-            }
-            int childIndex = 0;
-            for (Node referenceGraphChild : rootReferenceNode.childNodes()) {
-                if (!isSubTree(referenceGraphChild, rootSubTreeNode.childNodes().get(childIndex))) {
-                    return true;
+            } else {
+                logger.debug("Starting comparing children of {}", rootReferenceNode.getName());
+                int childIndex = 0;
+                boolean comparisonResult = true;
+                for (Node referenceGraphChild : rootReferenceNode.childNodes()) {
+                    comparisonResult = comparisonResult && isSubTree(referenceGraphChild, rootSubTreeNode.childNodes().get(childIndex));
+                    childIndex++;
                 }
-                childIndex++;
+                logger.debug("Comparison Result of childrens of {} is {}", rootReferenceNode.getName(), comparisonResult);
+                return comparisonResult;
             }
         }
         return rootReferenceNode.childNodes().stream().anyMatch((referenceGraphChild) -> (isSubTree(referenceGraphChild, rootSubTreeNode)));
