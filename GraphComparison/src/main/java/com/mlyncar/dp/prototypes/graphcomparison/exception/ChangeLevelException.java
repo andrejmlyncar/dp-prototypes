298d76ab51163b943237e1a4f08c77cbd8108094
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-10-29 18:14:04 +0200

    Added Change level enum.

---
+++ GraphComparison/src/main/java/com/mlyncar/dp/prototypes/graphcomparison/exception/ChangeLevelException.java
@@ -0,0 +1,16 @@
+package com.mlyncar.dp.prototypes.graphcomparison.exception;
+
+/**
+ *
+ * @author Andrej Mlyncar <a.mlyncar@gmail.com>
+ */
+public class ChangeLevelException extends Exception {
+
+    public ChangeLevelException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ChangeLevelException(String message) {
+        super(message);
+    }
+}
