9fc5299537980e3ac2f58c698c318f601d2d4fc2
Andrej Mlyncar <a.mlyncar@gmail.com>
2016-10-29 16:42:50 +0200

    Removed license header from comparisonserviceimpl and comparator class.

--- GraphComparison/src/main/java/com/mlyncar/dp/prototypes/graphcomparison/service/impl/ComparisonServiceImpl.java
+++ GraphComparison/src/main/java/com/mlyncar/dp/prototypes/graphcomparison/service/impl/ComparisonServiceImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mlyncar.dp.prototypes.graphcomparison.service.impl;
 
 import com.mlyncar.dp.prototypes.graphcomparison.change.Change;
